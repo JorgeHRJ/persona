90619eb00793d17792728fdce428b9c7288b94cd
Jorge Hernandez <jorge.hernandez@acilia.es>
2021-03-11 22:29:04 +0100

    deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -11,13 +11,11 @@ docker-compose down
 docker-compose up -d --build
 cd ../../../
 
-cp infra/env/.env.prod .env.local
+docker exec persona-php composer dump-env prod
+docker exec persona-php composer install --no-dev --optimize-autoloader
+docker exec persona-php bin/console doctrine:schema:update --force
+docker run -v $(pwd):/home/app persona-node yarn install
+docker run -v $(pwd):/home/app persona-node yarn encore production
 
-docker exec toolbox-php composer dump-env prod
-docker exec toolbox-php composer install --no-dev --optimize-autoloader
-docker exec toolbox-php bin/console doctrine:schema:update --force
-docker run -v $(pwd):/home/app toolbox-node yarn install
-docker run -v $(pwd):/home/app toolbox-node yarn encore production
-
-docker exec toolbox-php bin/console cache:clear
-docker exec toolbox-php bin/console cache:warmup
+docker exec persona-php bin/console cache:clear
+docker exec persona-php bin/console cache:warmup
